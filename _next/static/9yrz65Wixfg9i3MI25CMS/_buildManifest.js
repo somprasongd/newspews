8d07cd45afd9d96afe79090fdb9313c1cb5123c5
somprasongd <somprasongd@users.noreply.github.com>
2022-02-05 16:01:31 +0000

    Automated deployment: Sat Feb  5 16:01:31 UTC 2022 6cb908d6f047c3901d36a05b9f7682bfbe9e788d

---
+++ _next/static/9yrz65Wixfg9i3MI25CMS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/302-f27123b3378608e1.js","static/chunks/pages/index-11e2b8ca5db2ec8e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
